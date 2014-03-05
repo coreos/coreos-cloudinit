20df7b6a19fc732b3c24d83f015035911b6bd1b3
Brian Waldon <bcwaldon@gmail.com>
2014-03-04 17:01:58 -0800

    feat(version): Print binary version when given --version flag

--- coreos-cloudinit.go
+++ coreos-cloudinit.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"flag"
 	"io/ioutil"
+	"os"
 	"log"
 
 	"github.com/coreos/coreos-cloudinit/cloudinit"
 )
 
+const version = "0.1.0+git"
+
 func main() {
 	var userdata []byte
 	var err error
+
+	var printVersion bool
+	flag.BoolVar(&printVersion, "version", false, "Print the version and exit")
 
 	var file string
 	flag.StringVar(&file, "from-file", "", "Read user-data from file rather than metadata service")
@@ -19,6 +26,11 @@ func main() {
 	flag.StringVar(&workspace, "workspace", "/var/lib/coreos-cloudinit", "Base directory coreos-cloudinit should use to store data")
 
 	flag.Parse()
+
+	if printVersion == true {
+		fmt.Printf("coreos-cloudinit version %s\n", version)
+		os.Exit(0)
+	}
 
 	if file != "" {
 		log.Printf("Reading user-data from file: %s", file)
