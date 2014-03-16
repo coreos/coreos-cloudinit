86f8231264d65d121f161781a487471a44b62f5c
Alex Polvi <alex@polvi.net>
2014-03-16 16:22:27 -0700

    fix typo

--- cloudinit/user_test.go
+++ cloudinit/user_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-func TestCloudConfigUsersGithuMarshall(t *testing.T) {
+func TestCloudConfigUsersGithubMarshall(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		gh_res := `
 [
