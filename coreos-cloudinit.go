e317c7eb9aad0c10dc027c01fba3947a5f331b3d
Michael Marineau <michael.marineau@coreos.com>
2014-05-27 14:02:11 -0700

    chore(coreos-cloudinit): bump to 0.7.2+git

--- coreos-cloudinit.go
+++ coreos-cloudinit.go
@@ -11,7 +11,7 @@ import (
 	"github.com/coreos/coreos-cloudinit/system"
 )
 
-const version = "0.7.2"
+const version = "0.7.2+git"
 
 func init() {
 	//Removes timestamp since it is displayed already during booting
