d2a19cc86d0d5e6f5686963075306746b69bc2d5
Alex Crawford <alex.crawford@coreos.com>
2014-11-21 15:01:23 -0800

    config/flannel: correct - vs _

--- config/flannel.go
+++ config/flannel.go
@@ -1,9 +1,9 @@
 package config
 
 type Flannel struct {
-	EtcdEndpoint string `yaml:"etcd-endpoint" env:"FLANNELD_ETCD_ENDPOINT"`
-	EtcdPrefix   string `yaml:"etcd-prefix"   env:"FLANNELD_ETCD_PREFIX"`
-	IPMasq       string `yaml:"ip-masq"       env:"FLANNELD_IP_MASQ"`
-	SubnetFile   string `yaml:"subnet-file"   env:"FLANNELD_SUBNET_FILE"`
+	EtcdEndpoint string `yaml:"etcd_endpoint" env:"FLANNELD_ETCD_ENDPOINT"`
+	EtcdPrefix   string `yaml:"etcd_prefix"   env:"FLANNELD_ETCD_PREFIX"`
+	IPMasq       string `yaml:"ip_masq"       env:"FLANNELD_IP_MASQ"`
+	SubnetFile   string `yaml:"subnet_file"   env:"FLANNELD_SUBNET_FILE"`
 	Iface        string `yaml:"interface"     env:"FLANNELD_IFACE"`
 }
