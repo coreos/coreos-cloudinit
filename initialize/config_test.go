coreos/coreos-cloudinit

commit 9fcf338bf3e6c941068a2ce9e6ce64fbb3e4b65d

units: Ensure that the units are executed in order

initialize/config.go
@@ -291,7 +291,11 @@ func Apply(cfg CloudConfig, env *Environment) error {
 // disk, masking/unmasking units, or invoking systemd
 // commands against units. It returns any error encountered.
 func processUnits(units []system.Unit, root string, um system.UnitManager) error {
-	commands := make(map[string]string, 0)
+	type action struct {
+		unit    string
+		command string
+	}
+	actions := make([]action, 0, len(units))
 	reload := false
 	for _, unit := range units {
 		dst := unit.Destination(root)
@@ -329,9 +333,9 @@ func processUnits(units []system.Unit, root string, um system.UnitManager) error
 		}
 
 		if unit.Group() == "network" {
-			commands["systemd-networkd.service"] = "restart"
+			actions = append(actions, action{"systemd-networkd.service", "restart"})
 		} else if unit.Command != "" {
-			commands[unit.Name] = unit.Command
+			actions = append(actions, action{unit.Name, unit.Command})
 		}
 	}
 
@@ -341,13 +345,13 @@ func processUnits(units []system.Unit, root string, um system.UnitManager) error
 		}
 	}
 
-	for unit, command := range commands {
-		log.Printf("Calling unit command '%s %s'", command, unit)
-		res, err := um.RunUnitCommand(command, unit)
+	for _, action := range actions {
+		log.Printf("Calling unit command '%s %s'", action.command, action.unit)
+		res, err := um.RunUnitCommand(action.command, action.unit)
 		if err != nil {
 			return err
 		}
-		log.Printf("Result of '%s %s': %s", command, unit, res)
+		log.Printf("Result of '%s %s': %s", action.command, action.unit, res)
 	}
 
 	return nil

commit 2270db3f7a784dde3ed3023d300b8de765224ecd

initialize: only restart networkd once per config
Regression introduced by 9fcf338bf3e6c941068a2ce9e6ce64fbb3e4b65d.

Networkd was erroneously being restarted once per network unit. It
should only restart once for the entire config.

initialize/config.go
@@ -200,6 +200,7 @@ func processUnits(units []system.Unit, root string, um system.UnitManager) error
 	}
 	actions := make([]action, 0, len(units))
 	reload := false
+	restartNetworkd := false
 	for _, unit := range units {
 		if unit.Name == "" {
 			log.Printf("Skipping unit without name")
@@ -251,8 +252,7 @@ func processUnits(units []system.Unit, root string, um system.UnitManager) error
 		}
 
 		if unit.Group() == "network" {
-			networkd := system.Unit{Unit: config.Unit{Name: "systemd-networkd.service"}}
-			actions = append(actions, action{networkd, "restart"})
+			restartNetworkd = true
 		} else if unit.Command != "" {
 			actions = append(actions, action{unit, unit.Command})
 		}
@@ -262,6 +262,11 @@ func processUnits(units []system.Unit, root string, um system.UnitManager) error
 		if err := um.DaemonReload(); err != nil {
 			return errors.New(fmt.Sprintf("failed systemd daemon-reload: %s", err))
 		}
+	}
+
+	if restartNetworkd {
+		networkd := system.Unit{Unit: config.Unit{Name: "systemd-networkd.service"}}
+		actions = append(actions, action{networkd, "restart"})
 	}
 
 	for _, action := range actions {

initialize/config_test.go
@@ -29,8 +29,13 @@ type TestUnitManager struct {
 	enabled  []string
 	masked   []string
 	unmasked []string
-	commands map[string]string
+	commands []UnitAction
 	reload   bool
+}
+
+type UnitAction struct {
+	unit    string
+	command string
 }
 
 func (tum *TestUnitManager) PlaceUnit(u system.Unit) error {
@@ -46,8 +51,7 @@ func (tum *TestUnitManager) EnableUnitFile(u system.Unit) error {
 	return nil
 }
 func (tum *TestUnitManager) RunUnitCommand(u system.Unit, c string) (string, error) {
-	tum.commands = make(map[string]string)
-	tum.commands[u.Name] = c
+	tum.commands = append(tum.commands, UnitAction{u.Name, c})
 	return "", nil
 }
 func (tum *TestUnitManager) DaemonReload() error {
@@ -83,12 +87,15 @@ func TestProcessUnits(t *testing.T) {
 		{
 			units: []system.Unit{
 				system.Unit{Unit: config.Unit{
+					Name: "foo.network",
+				}},
+				system.Unit{Unit: config.Unit{
 					Name: "bar.network",
 				}},
 			},
 			result: TestUnitManager{
-				commands: map[string]string{
-					"systemd-networkd.service": "restart",
+				commands: []UnitAction{
+					UnitAction{"systemd-networkd.service", "restart"},
 				},
 			},
 		},