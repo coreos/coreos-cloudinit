112ba1e31f908a426764f5a854dbc3cc79585f6e
cnelson <cnelson@cnelson.org>
2014-12-09 08:10:23 -0800

    Added support for the encoding key in write_files
    Supported encodings are base64, gzip, and base64 encoded gzip

--- system/file_test.go
+++ system/file_test.go
@@ -156,10 +156,97 @@ func TestWriteFileEncodedContent(t *testing.T) {
 	}
 	defer os.RemoveAll(dir)
 
+	//all of these decode to "bar"
+	content_tests := map[string]string{
+		"base64":      "YmFy",
+		"b64":         "YmFy",
+		"gz":          "\x1f\x8b\x08\x08w\x14\x87T\x02\xffok\x00KJ,\x02\x00\xaa\x8c\xffv\x03\x00\x00\x00",
+		"gzip":        "\x1f\x8b\x08\x08w\x14\x87T\x02\xffok\x00KJ,\x02\x00\xaa\x8c\xffv\x03\x00\x00\x00",
+		"gz+base64":   "H4sIABMVh1QAA0tKLAIAqoz/dgMAAAA=",
+		"gzip+base64": "H4sIABMVh1QAA0tKLAIAqoz/dgMAAAA=",
+		"gz+b64":      "H4sIABMVh1QAA0tKLAIAqoz/dgMAAAA=",
+		"gzip+b64":    "H4sIABMVh1QAA0tKLAIAqoz/dgMAAAA=",
+	}
+
+	for encoding, content := range content_tests {
+		fullPath := path.Join(dir, encoding)
+
+		wf := File{config.File{
+			Path:               encoding,
+			Encoding:           encoding,
+			Content:            content,
+			RawFilePermissions: "0644",
+		}}
+
+		path, err := WriteFile(&wf, dir)
+		if err != nil {
+			t.Fatalf("Processing of WriteFile failed: %v", err)
+		} else if path != fullPath {
+			t.Fatalf("WriteFile returned bad path: want %s, got %s", fullPath, path)
+		}
+
+		fi, err := os.Stat(fullPath)
+		if err != nil {
+			t.Fatalf("Unable to stat file: %v", err)
+		}
+
+		if fi.Mode() != os.FileMode(0644) {
+			t.Errorf("File has incorrect mode: %v", fi.Mode())
+		}
+
+		contents, err := ioutil.ReadFile(fullPath)
+		if err != nil {
+			t.Fatalf("Unable to read expected file: %v", err)
+		}
+
+		if string(contents) != "bar" {
+			t.Fatalf("File has incorrect contents: '%s'", contents)
+		}
+	}
+}
+
+func TestWriteFileInvalidEncodedContent(t *testing.T) {
+	dir, err := ioutil.TempDir(os.TempDir(), "coreos-cloudinit-")
+	if err != nil {
+		t.Fatalf("Unable to create tempdir: %v", err)
+	}
+	defer os.RemoveAll(dir)
+
+	content_encodings := []string{
+		"base64",
+		"b64",
+		"gz",
+		"gzip",
+		"gz+base64",
+		"gzip+base64",
+		"gz+b64",
+		"gzip+b64",
+	}
+
+	for _, encoding := range content_encodings {
+		wf := File{config.File{
+			Path:     path.Join(dir, "tmp", "foo"),
+			Content:  "@&*#%invalid data*@&^#*&",
+			Encoding: encoding,
+		}}
+
+		if _, err := WriteFile(&wf, dir); err == nil {
+			t.Fatalf("Expected error to be raised when writing file with encoding")
+		}
+	}
+}
+
+func TestWriteFileUnknownEncodedContent(t *testing.T) {
+	dir, err := ioutil.TempDir(os.TempDir(), "coreos-cloudinit-")
+	if err != nil {
+		t.Fatalf("Unable to create tempdir: %v", err)
+	}
+	defer os.RemoveAll(dir)
+
 	wf := File{config.File{
 		Path:     path.Join(dir, "tmp", "foo"),
 		Content:  "",
-		Encoding: "base64",
+		Encoding: "no-such-encoding",
 	}}
 
 	if _, err := WriteFile(&wf, dir); err == nil {
