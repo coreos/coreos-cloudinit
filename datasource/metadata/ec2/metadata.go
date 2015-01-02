13e4b7713015a255c55a7c0d058a8586977f890f
Kevin Yung <kevin.yung@rea-group.com>
2015-01-02 11:38:37 +1100

    ec2: allow spaces seperated hostname in metadata
    AWS hostname metadata will return space seperated hostname and domain
    names when DHCPOptionSet is using multiple domain names. This patch will
    cater for this scenario.

--- datasource/metadata/ec2/metadata.go
+++ datasource/metadata/ec2/metadata.go
@@ -69,7 +69,7 @@ func (ms metadataService) FetchMetadata() ([]byte, error) {
 	}
 
 	if hostname, err := ms.fetchAttribute(fmt.Sprintf("%s/hostname", ms.MetadataUrl())); err == nil {
-		attrs["hostname"] = hostname
+		attrs["hostname"] = strings.Split(hostname, " ")[0]
 	} else if _, ok := err.(pkg.ErrNotFound); !ok {
 		return nil, err
 	}
