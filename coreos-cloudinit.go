ae3676096cabaaa5a65c4976ba31983833fb09ee
Alex Crawford <alex.crawford@coreos.com>
2015-01-21 14:29:25 -0800

    coreos-cloudinit: bump to v1.2.1

--- coreos-cloudinit.go
+++ coreos-cloudinit.go
@@ -40,7 +40,7 @@ import (
 )
 
 const (
-	version               = "1.2.0+git"
+	version               = "1.2.1"
 	datasourceInterval    = 100 * time.Millisecond
 	datasourceMaxInterval = 30 * time.Second
 	datasourceTimeout     = 5 * time.Minute
